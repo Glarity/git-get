09ef5bfcb463eae3d33c0ca9ab8c7db868b79f2c
Glarity <tolson519@gmail.com>
2018-05-19 02:05:45 -0700

    Added second argument to choose

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # git-get
 Gets git info (hehe)
+
+#Todo
+* Add list of possible queries
+* Tab-auto-complete possibilities
+* Run outside of project folder?
+* Be able to use in other projects for quick queries?
--- get-git.sh
+++ get-git.sh
@@ -2,15 +2,15 @@
 #curl -i https://api.github.com/users/glarity
 
 function checkUser() {
-		CURL="curl -s -i https://api.github.com/users/$1"
-		PART="name"
-		OUTPUT="$($CURL | jq .$PART)"
+		CURL="curl -s https://api.github.com/users/$1"
+		PART=$2
+		OUTPUT="$($CURL | jq -r .$PART)"
 		echo ${OUTPUT}
 	}
 
-if [ $# -ne 0 ]; then
-	checkUser "$1"
-elif [ $# -eq 0 ]; then
+if [ $# -gt 1 ]; then
+	checkUser "$1" "$2"
+elif [ $# -lt 2 ]; then
 	#statements
-	echo "Please enter a user"
+	echo "Please enter a user and object."
 fi
